18b893b5558b8ab6a4fec51692957b9a628a7f59
paula hage <hage.paula@hotmail.com>
2023-06-12 17:51:33 +0200

    Implemented logic on class component + event binding

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NumberFormatStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,21 +9,48 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
-  errorMessage = "";
+  errorMessage = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   onInput(member: string) {
     this.newMemberName = member;
-    console.log(this.newMemberName);
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
   }
 
   addMember() {
     if (!this.newMemberName) {
-      this.errorMessage = "Name can't be empty"
+      this.errorMessage = "Name can't be empty";
       return;
     }
     this.members.push(this.newMemberName);
-    this.newMemberName = "";
-    this.errorMessage = "";
-    console.log('added');
+    this.newMemberName = '';
+    this.errorMessage = '';
+  }
+
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      return;
+    }
+
+    const allMembersCopy = [...this.members];
+
+    while (allMembersCopy.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembersCopy.length);
+        const member = allMembersCopy.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
   }
 }
