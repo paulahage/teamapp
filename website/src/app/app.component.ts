3e9c34192088cedcc0207613f56911073a28bf81
paula hage <hage.paula@hotmail.com>
2023-06-12 16:42:45 +0200

    Implemented state and methods on the class component + added structural and attribute directives

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'website';
+  newMemberName = '';
+  members: string[] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+  }
 }
